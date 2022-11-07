61d2cb4078c033479144273aa17c44a7a17a91eb
Samson Dupuy <samson.dupuy@edu.univ-fcomte.fr>
2022-11-07 10:16:18 +0100

    test git 2

--- routers/laureates.router.js
+++ routers/laureates.router.js
@@ -69,9 +69,55 @@ module.exports = app => {
     router.get("/page/:page", laureates.findPage);
     /**
      * @swagger
-     * /laureates/page/:
+     * /laureates/page/{page}:
      *   get:
-     *      description: Used to get all people that have multiple laureates
+     *      description: Used to get a page of laureat
+     *      tags:
+     *          - id,
+     *            firstname,
+     *            surname,
+     *            number
+     *      parameters:
+     *          - in: path
+     *            name: page
+     *            schema:
+     *              type: integer
+     *            required: true
+     *      responses:
+     *          '200':
+     *              description: Succes
+     *          '500':
+     *              description: Internal server error
+     *          '400':
+     *              description: Bad request
+     */
+
+    router.get("/year", laureates.findYear);
+     /**
+     * @swagger
+     * /laureates/year/:
+     *   get:
+     *      description: Used to get all years and the number of laureate given in that year
+     *      tags:
+     *          - id,
+     *            firstname,
+     *            surname,
+     *            number
+     *      responses:
+     *          '200':
+     *              description: Succes
+     *          '500':
+     *              description: Internal server error
+     *          '400':
+     *              description: Bad request
+     */
+    
+    router.get("/noYear", laureates.findYearNoLaureate);
+    /**
+     * @swagger
+     * /laureates/noYear/:
+     *   get:
+     *      description: Used to get all years that don't have laureate
      *      tags:
      *          - id,
      *            firstname,
@@ -86,13 +132,68 @@ module.exports = app => {
      *              description: Bad request
      */
 
-    router.get("/year", laureates.findYear);
-    
-    router.get("/noYear", laureates.findYearNoLaureate);
-
     router.get("/year/sort/:signe", laureates.orderlaureate);
+    /**
+     * @swagger
+     * /laureates/year/sort/{signe}:
+     *   get:
+     *      description: Used to order year by they number of laureate (- for asc and + for desc)
+     *      tags:
+     *          - id,
+     *            firstname,
+     *            surname,
+     *            number
+     *      parameters:
+     *          - in: path
+     *            name: signe
+     *            schema:
+     *              type: string
+     *            required: true
+     *      responses:
+     *          '200':
+     *              description: Succes
+     *          '500':
+     *              description: Internal server error
+     *          '400':
+     *              description: Bad request
+     */
 
     router.get("/supprimer/:id/:annee/:categorie", laureates.delete);
+    /**
+     * @swagger
+     * /laureates/supprimer/{id]/{annee}/{categorie}:
+     *   get:
+     *      description: Used to delete a laureate from a specific year, a specific id and  a specific categorie
+     *      tags:
+     *          - id,
+     *            firstname,
+     *            surname,
+     *            number
+     *      parameters:
+     *          - in: path
+     *            name: id
+     *            schema:
+     *              type: integer
+     *            required: true
+     *          - in: path
+     *            name: anne
+     *            schema:
+     *              type: integer
+     *            required: true
+     *          - in: path
+     *            name: categorie
+     *            schema:
+     *              type: string
+     *            required: true
+     *      responses:
+     *          '200':
+     *              description: Succes
+     *          '500':
+     *              description: Internal server error
+     *          '400':
+     *              description: Bad request
+     */
+    
 
     router.get("/supprimer/:id/:annee/:categorie", laureates.add);
 
